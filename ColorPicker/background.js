92df6769fa8ee4013fd7aac992bf3099e0bdbcc0
Kami on YT <kamibusinessinquirers@gmail.com>
2023-08-02 21:35:34 +1000

    Delete ColorPicker directory

--- ColorPicker/background.js
+++
@@ -1,7 +0,0 @@
-// Download page
-chrome.runtime.onInstalled.addListener(function (object) {
-  let downloadPage = chrome.runtime.getURL("download.html");
-  if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    chrome.tabs.create({ url: downloadPage });
-  }
-});
